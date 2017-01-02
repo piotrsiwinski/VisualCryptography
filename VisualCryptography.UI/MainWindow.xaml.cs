0d7c27fa013c21a18ccb958e9bc7de672a6b8b5c
Piotrek <siwinski.piotr@outlook.com>
2017-01-02 14:55:56 +0100

    Added opening and saving images

--- VisualCryptography.UI/MainWindow.xaml.cs
+++ VisualCryptography.UI/MainWindow.xaml.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,6 +14,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Microsoft.Win32;
+using VisualCryptography.UI.Utils;
 
 namespace VisualCryptography.UI
 {
@@ -20,9 +24,68 @@ namespace VisualCryptography.UI
     /// </summary>
     public partial class MainWindow : Window
     {
+        private string _originalImagePath;
+
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void OpenOriginalImageButton_OnClick(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                var openFileDialog = new OpenFileDialog
+                {
+                    Title = "Select a picture",
+                    Filter = "All supported graphics|*.jpg;*.jpeg;*.png|" +
+                         "JPEG (*.jpg;*.jpeg)|*.jpg;*.jpeg|" +
+                         "Portable Network Graphic (*.png)|*.png"
+                };
+
+                if (openFileDialog.ShowDialog() != true) return;
+                _originalImagePath = openFileDialog.FileName;
+                OriginalImage.Source = new BitmapImage(new Uri(_originalImagePath));
+                FirstImage.Source = new BitmapImage(new Uri(_originalImagePath));
+                SecondImage.Source = new BitmapImage(new Uri(_originalImagePath));
+            }
+            catch (Exception exception)
+            {
+                MessageBox.Show($"Nie udało się zapisać do pliku. Szczegóły błędu: {exception.Message}");
+            }
+        }
+
+        private void SaveDecryptedImagesToFile_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                var firstImageBitmap = (FirstImage.Source as BitmapImage)?.ConvertToBitmap();
+                var secondImageBitmap = (SecondImage.Source as BitmapImage)?.ConvertToBitmap();
+                
+                var saveFileDialog = new SaveFileDialog
+                {
+                    Title = "Select a picture",
+                    Filter = "All supported graphics|*.jpg;*.jpeg;*.png|" +
+                         "JPEG (*.jpg;*.jpeg)|*.jpg;*.jpeg|" +
+                         "Portable Network Graphic (*.png)|*.png"
+                };
+
+                if (saveFileDialog.ShowDialog() != true) return;
+                firstImageBitmap?.Save(saveFileDialog.FileName);
+                secondImageBitmap?.Save(saveFileDialog.FileName);
+            }
+            catch (Exception exception)
+            {
+                MessageBox.Show($"Nie udało się zapisać do pliku. Szczegóły błędu: {exception.Message}");
+            }
+        }
+
+        private void EncryptImage_Click(object sender, RoutedEventArgs e)
+        {
+            if (FirstImage.Source != null && SecondImage.Source != null)
+            {
+                SaveDecryptedImagesToFile.IsEnabled = true;
+            }
         }
     }
 }
---
+++ VisualCryptography.UI/Utils/ImageUtils.cs
@@ -0,0 +1,20 @@
+﻿using System.Drawing;
+using System.IO;
+using System.Windows.Media.Imaging;
+
+namespace VisualCryptography.UI.Utils
+{
+    public static class ImageUtils
+    {
+        public static Bitmap ConvertToBitmap(this BitmapImage bitmapImage)
+        {
+            using (MemoryStream outStream = new MemoryStream())
+            {
+                BitmapEncoder enc = new BmpBitmapEncoder();
+                enc.Frames.Add(BitmapFrame.Create(bitmapImage)); enc.Save(outStream);
+                System.Drawing.Bitmap bitmap = new Bitmap(outStream);
+                return new Bitmap(bitmap);
+            }
+        }
+    }
+}
